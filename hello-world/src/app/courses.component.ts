1840b9a50b87bb2b2d3a228deea3b2cd252165a8
Alex Laursen <ajlaursen@aol.com>
2021-05-29 14:48:07 -0400

    event filtering

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -6,29 +6,37 @@ import { CoursesService } from './courses.service';
   selector: 'courses',
   template: `
     <h2>{{ title }}</h2>
-    <img [src]="imageUrl"/>
+    <img [src]="imageUrl" />
     <table>
-     <tr>
-        <td [attr.colSpan]="colSpan">
+      <tr>
+        <td [attr.colSpan]="colSpan"></td>
       </tr>
     </table>
-    <button class="btn btn-primary" [class.active]="isActive" (click)="onSave($event)" >Save</button>
+    <button
+      class="btn btn-primary"
+      [class.active]="isActive"
+      (click)="onSave($event)"
+    >
+      Save
+    </button>
+
+    <input (keyup.enter)="onKeyUp($event)"/>
   `,
 })
 export class CoursesComponent {
   title = 'List of courses';
   courses;
-  imageUrl = "https://via.placeholder.com/150"
-  colSpan = 2
-  isActive = true
+  imageUrl = 'https://via.placeholder.com/150';
+  colSpan = 2;
+  isActive = true;
   onSave($event: Event) {
-    console.log("button was clicked", $event)
+    $event.stopPropagation();
+    console.log('button was clicked', $event);
   }
-  
-  constructor(service: CoursesService){
-    
-    this.courses = service.getCourses()
+  onKeyUp($event: Event){
+    console.log("Enter was pressed")
   }
-
-
+  constructor(service: CoursesService) {
+    this.courses = service.getCourses();
+  }
 }
